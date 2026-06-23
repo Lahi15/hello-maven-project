fb66574121354a05628e5be715d511154e403240
Lahitha Reddy <lahithareddy.k15@gmail.com>
2026-06-23 06:53:26 +0530

    Initial Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App{
+	public static void main(String[] args){
+		System.out.println("Hello Azure pipeline");
+	}
+	public static int add(int a,int b){
+		return a+b;
+	}
+}
